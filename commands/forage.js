608c31107e2418138ff3b565106c534f74825584
EpixOce <34924285+EpixOce@users.noreply.github.com>
2018-05-19 18:39:44 +1200

    Create forage.js

---
+++ commands/forage.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    setTimeout(function() {
+        message.delete();
+    }, 10000)
+}
+
+module.exports.help = {
+    name: "forage"
+}
